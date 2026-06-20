c01d7d7c5f406c2707b31b883d5e37b121369dba
A-CHINMAY <a.chinmay2005@gmail.com>
2026-06-20 20:22:27 +0530

    updated

--- prog7.py
+++ prog7.py
@@ -2,63 +2,59 @@ from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
-from sklearn.metrics import *
+from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Linear Regression - Boston Dataset
-boston = fetch_openml(name="boston", version=1, as_frame=True)
-X, y = boston.data, boston.target.astype(float)
+# ---------- Linear Regression ----------
+data = fetch_openml("boston", version=1, as_frame=True)
+
+X = pd.get_dummies(data.data).astype(float)
+y = data.target.astype(float)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    X, y, test_size=0.2, random_state=42)
 
-model = LinearRegression()
-model.fit(X_train, y_train)
-y_pred = model.predict(X_test)
+model = LinearRegression().fit(X_train, y_train)
+pred = model.predict(X_test)
 
-print("----- Linear Regression -----")
-print("MSE:", mean_squared_error(y_test, y_pred))
-print("R2 Score:", r2_score(y_test, y_pred))
+print("Linear Regression")
+print("MSE:", mean_squared_error(y_test, pred))
+print("R2:", r2_score(y_test, pred))
 
-plt.scatter(y_test, y_pred)
-plt.xlabel("Actual Prices")
-plt.ylabel("Predicted Prices")
-plt.title("Actual vs Predicted")
+plt.scatter(y_test, pred)
+plt.xlabel("Actual")
+plt.ylabel("Predicted")
 plt.show()
 
-# Polynomial Regression - Auto MPG Dataset
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data"
+# ---------- Polynomial Regression ----------
+cols = ["mpg","cylinders","displacement","horsepower",
+        "weight","acceleration","model_year","origin","car_name"]
 
-cols = ['mpg','cylinders','displacement','horsepower',
-        'weight','acceleration','model_year','origin','car_name']
+auto = pd.read_csv(
+    "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data",
+    names=cols, na_values="?", sep=r"\s+"
+).dropna()
 
-auto = pd.read_csv(url, names=cols, na_values='?',
-                   sep=' ', skipinitialspace=True).dropna()
-
-X = auto[['horsepower']]
-y = auto['mpg']
+X = auto[["horsepower"]]
+y = auto["mpg"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+    X, y, test_size=0.2, random_state=42)
 
 poly = PolynomialFeatures(2)
+
 X_train = poly.fit_transform(X_train)
 X_test = poly.transform(X_test)
 
-model = LinearRegression()
-model.fit(X_train, y_train)
+model = LinearRegression().fit(X_train, y_train)
+pred = model.predict(X_test)
 
-y_pred = model.predict(X_test)
+print("\nPolynomial Regression")
+print("MSE:", mean_squared_error(y_test, pred))
+print("R2:", r2_score(y_test, pred))
 
-print("\n----- Polynomial Regression -----")
-print("MSE:", mean_squared_error(y_test, y_pred))
-print("R2 Score:", r2_score(y_test, y_pred))
-
-plt.scatter(auto['horsepower'], auto['mpg'])
+plt.scatter(auto["horsepower"], auto["mpg"])
 plt.xlabel("Horsepower")
 plt.ylabel("MPG")
-plt.title("Polynomial Regression Fit")
 plt.show()
